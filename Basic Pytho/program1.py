1433147190962d4c22e922e55917630bf83d7cca
Md Shakib Al Arman <your-email@gmail.com>
2026-01-01 07:36:06 -0800

    initial commit

---
+++ Basic Pytho/program1.py
@@ -0,0 +1,13 @@
+name = "Md Shakib Al Arman"
+age = 20
+country = "Bangladesh"
+profession = "Student"
+email = "shakibalarman.cse@gmail.com"
+phone = "018219396**"
+
+print("My name is:", name)
+print("I am", age, "years old.")
+print("I live in ", country)
+print("I am a ", profession)
+print("Email:", email)
+print("Phone:", phone)
