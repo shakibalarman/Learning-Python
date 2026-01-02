be3162c990f372bd025b038d6ccbb6fa69be4f7c
Md Shakib Al Arman <your-email@gmail.com>
2026-01-02 15:48:18 -0800

    smart traffic signal

---
+++ Basic Python/Day2/problem2.py
@@ -0,0 +1,12 @@
+color = input('Enter the color ')
+color1 = "red"
+color2 = "yellow"
+color3 = "green"
+if(color == color1):
+    print("Stop")
+elif(color == color2):
+    print("Get ready")
+elif(color == color3):
+    print("Go ")
+else:
+    print ("Invalid signal")
