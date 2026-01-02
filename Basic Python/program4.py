a74f820b8ca8855ad5ffc015fc17811fa6f2c5fe
Md Shakib Al Arman <your-email@gmail.com>
2026-01-01 18:02:57 -0800

    Trying if, else

--- Basic Python/program4.py
+++ Basic Python/program4.py
@@ -1,5 +1,11 @@
 year = int(input("Enter a year :"))
 if (year % 4 == 0):
     print("{0} is a leap year ".format(year))
+
+elif (year % 100 == 0):
+    print("{0} is leap year ".format(year))
 else:
-    print("{0} is not a leap year ".format(year))    
+    print("{0} is not a leap year ".format(year))  
+     
+  
+ 
