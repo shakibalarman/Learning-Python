629536d7848cee70b5c6addbaeca2cb12a55d88d
Md Shakib Al Arman <your-email@gmail.com>
2026-01-01 20:08:37 -0800

    largest among three number

---
+++ Basic Python/program6.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number for a = "))
+b = int(input("Enter a number for b = "))
+c = int(input("Enter a number for c = "))
+if (a>b and (a>c)):
+    print("a is the largest number ")
+elif (b>a and (b>c)):
+    print("b is the largest number ")
+else:
+    print("c is the largest number ")        
