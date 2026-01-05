b6f81169c4f84cc67b68cbbc25d1c46f302c5e5e
Md Shakib Al Arman <your-email@gmail.com>
2026-01-06 05:52:24 +0600

    Implement menu-based utility program using functions and loops

--- Basic Python/Day5-function/max-of-three-number.py
+++ Basic Python/Day5-function/max-of-three-number.py
@@ -7,3 +7,14 @@ def max_of_three(a,b,c):
         return c
 x = max_of_three(10,20,30)
 print(x)
+
+while True:
+    print("\n---Utiliti Menu---")
+    print("1. Check Even or Odd")
+    print("2. Find square")
+    print("3. Find largest of two number ")
+    print("4. Exit ")
+    choice = int(input("Enter your choice "))
+    if choice == 4 :
+        print("Thank You! ")
+        break 
