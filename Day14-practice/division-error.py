edb1f2ceb44ae1157f6d0f1ba829c46622f7183f
shakibalarman <shakibalarman.cse@gmail.com>
2026-01-30 23:54:27 +0600

    division error handling

---
+++ Day14-practice/division-error.py
@@ -0,0 +1,12 @@
+try:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    result = num1 / num2
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero!")
+
+except ValueError:
+    print("Error: Please enter valid numbers!")
