9be0f4d62fc30aab10416fd7fda0cddc13653d15
shakibalarman <shakibalarman.cse@gmail.com>
2026-02-01 01:33:23 +0600

    Update voter function test case to check for valid age

--- Day15-practice/raise-function.py
+++ Day15-practice/raise-function.py
@@ -3,6 +3,6 @@ def voter(age):
         raise ValueError ("Invalid Voter")
     return("You are allowed to vote ")
 try:
-    print(voter (18))
+    print(voter (19))
 except ValueError as e:
     print (e)
