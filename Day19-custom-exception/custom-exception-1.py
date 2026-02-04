7f0949581b60675397a27ff13e0c7249f621c0f9
shakibalarman <shakibalarman.cse@gmail.com>
2026-02-04 20:29:12 +0600

    Add custom exception for age validation in voting eligibility

---
+++ Day19-custom-exception/custom-exception-1.py
@@ -0,0 +1,12 @@
+class InvalidAgeException(Exception):
+    "Raised when age is less than 18 "
+    pass
+number = 18
+try:
+    age = int(input("Enter your age "))
+    if age < number:
+        raise InvalidAgeException
+    else:
+        print("Eligible to vote ")
+except InvalidAgeException:
+    print("Exception is occured: Invalid Age ")
