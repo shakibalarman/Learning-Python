2a279789abcb8ab8367edb5bc66cfba0adec7d62
shakibalarman <shakibalarman.cse@gmail.com>
2026-02-18 03:35:28 +0600

    I made a simple calculator with error handle

---
+++ Day24-practice/mini-calculator.py
@@ -0,0 +1,15 @@
+try:
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    sum = a+b
+    print("The summation is ", sum )
+    sub = a - b
+    print("The substruction is ", sub )
+    multiplication = a*b
+    print("The multiplication is ", multiplication)
+    division = a/b
+    print ("The division is ", division )
+except ZeroDivisionError:
+    print("infinity")
+except ValueError:
+    print("Error: Pls enter a numeric degit ")
