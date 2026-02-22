0d8be08e7506ec42edb73c998bbbb9557a91306e
shakibalarman <shakibalarman.cse@gmail.com>
2026-02-22 20:46:17 +0600

    Add basic arithmetic operation with error handling for numeric input

---
+++ Day26/first.py
@@ -0,0 +1,7 @@
+try:
+    a = int(input("Enter a number "))
+    b = int(input("Enter another number "))
+    result = a + b 
+    print("The summation is ",result)
+except ValueError:
+    print("Error: enter the numeric value only ")
